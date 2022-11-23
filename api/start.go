0b187177e2452cfb5a3752045d6e18337703261d
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-11-23 14:49:19 -0600

    Reorganized file structure

---
+++ api/start.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/martoranam/AngularFront/client"
+)
+
+func Start() {
+	router := gin.Default()
+
+	client.AddRoutes(router)
+
+	router.Run(":5000")
+}
